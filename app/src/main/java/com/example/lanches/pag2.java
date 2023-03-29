75c372ee22ddd3d84d96748b8d4a752a477d91d2
cadamowicz <claudio@cwbtech.com.br>
2023-03-28 21:43:34 -0300

    pag02 ok

--- app/src/main/java/com/example/lanches/pag2.java
+++ app/src/main/java/com/example/lanches/pag2.java
@@ -2,6 +2,7 @@ package com.example.lanches;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +12,12 @@ import android.widget.TextView;
 public class pag2 extends AppCompatActivity {
 
     EditText oplanche;
+    EditText qtde;
     TextView sub1;
     Button b2;
+    Button b3;
+
+    float subtotal=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class pag2 extends AppCompatActivity {
         oplanche=findViewById(R.id.oplanche);
         sub1=findViewById(R.id.sub1);
         b2=findViewById(R.id.b2);
+        qtde=findViewById(R.id.qtde);
+        b3=findViewById(R.id.b3);
 
 
         b2.setOnClickListener(new View.OnClickListener() {
@@ -28,34 +35,43 @@ public class pag2 extends AppCompatActivity {
             public void onClick(View view) {
                 String op1 = oplanche.getText().toString();
                 int opcao1 = Integer.parseInt(op1);
+                String qtd=qtde.getText().toString();
+                int quantidade=Integer.parseInt(qtd);
 
-                float subtotal=0;
+
                 switch (opcao1) {
                     case 1:
-                        subtotal=10;
+                        subtotal=subtotal+10*quantidade;
                         sub1.setText(String.valueOf("R$" + subtotal));
                         break;
                     case 2:
-                        System.out.println("Tuesday");
+                        subtotal=subtotal+15*quantidade;
+                        sub1.setText(String.valueOf("R$" + subtotal));
                         break;
                     case 3:
-                        System.out.println("Wednesday");
+                        subtotal=subtotal+18*quantidade;
+                        sub1.setText(String.valueOf("R$" + subtotal));
                         break;
                     case 4:
-                        System.out.println("Thursday");
+                        subtotal=subtotal+25*quantidade;
+                        sub1.setText(String.valueOf("R$" + subtotal));
                         break;
                 }
 
 
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-
-
-                // etresultado.setText(String.valueOf(somatotal));
-
+                Intent intent1 = new Intent(pag2.this, pag3.class);
+                intent1.putExtra("totalpag2",subtotal);
+                startActivity(intent1);
 
 
             }
         });
+
     }
 }
