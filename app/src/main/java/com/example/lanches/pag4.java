9f9d1af0b963ec7675ff62e309e5bcf37d1bbb92
Joice-Opolis <joice.opolis@gmail.com>
2023-03-31 13:11:45 -0300

    botão pag 4

--- app/src/main/java/com/example/lanches/pag4.java
+++ app/src/main/java/com/example/lanches/pag4.java
@@ -2,13 +2,28 @@ package com.example.lanches;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class pag4 extends AppCompatActivity {
+    Button b6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pag4);
+        b6=findViewById(R.id.b6);
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(pag4.this, MainActivity.class);
+            }
+        });
     }
+
+
+
 }
